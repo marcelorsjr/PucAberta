c9debac4fc107538d0696e839083e4517aa17696
Marcelo Rodrigues dos Santos Junior <marrsantosjunior@gmail.com>
2017-05-05 00:06:19 -0300

    - Merge

--- Puc Aberta/ViewControllers/AboutViewController.swift
+++ Puc Aberta/ViewControllers/AboutViewController.swift
@@ -15,7 +15,6 @@ class AboutViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
